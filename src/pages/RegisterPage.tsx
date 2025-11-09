42e6709924a81208f0af82a6946436764a99b0a7
mikulec.marin52 <mikulec.marin52@gmail.com>
2025-11-09 10:26:11 +0000

    register

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
 const RegisterPage: React.FC = () => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setUser(prevState => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:8080/api/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch(e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="form-wrapper">
       <div className="form-container">
         <h2>Register</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter your username"
+              value={user.username}
+              onChange={handleChange}
+            />
+          </div>
           <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Enter your email" />
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              value={user.email}
+              onChange={handleChange}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="Enter your password" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="confirm-password">Confirm Password</label>
-            <input type="password" id="confirm-password" placeholder="Confirm your password" />
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              value={user.password}
+              onChange={handleChange}
+            />
           </div>
           <button type="submit" className="btn">Register</button>
         </form>
